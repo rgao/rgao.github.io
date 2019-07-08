8fffc556a474f754fe21787553c0b7da2b08c490
rgao <xinghoshi@gmail.com>
2019-07-07 21:21:51 -0700

    finished css design of main page

---
+++ assets/logic.js
@@ -0,0 +1,17 @@
+// $(window).on('resize', function() {
+
+//     if ($(this).width() >= 534) {
+//       $('#header-margin').addClass('col-4').removeClass('col-2');
+//       $('#empty-div').css('height', '45vh');
+//       $('#empty-div2').css('height', '0vh');
+//       $("#menu-border-top").css('width', '45vw');
+//       $("#menu-border-top").css('min-width', '534px');
+  
+//     } else {
+//       $('#header-margin').addClass('col-2').removeClass('col-4');
+//       $('#empty-div').css('height', '15vh');
+//       $('#empty-div2').css('height', '23vh');
+//       $("#menu-border-top").css('width', '90vw');
+//       $("#menu-border-top").css('min-width', '90vw');
+//     }
+//   });
